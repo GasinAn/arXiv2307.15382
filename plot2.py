cec3207c54ac261802612268881b53f5d37d1bb8
GasinAn <Gasin185@163.com>
2023-02-27 21:52:03 +0800

    replot plot2, new method

--- plot2.py
+++ plot2.py
@@ -32,110 +32,88 @@ for i in range(lamdas.size):
 qcs = plt.contourf(lamdas, alphas, p_alphas, levels=1001)
 plt.colorbar(qcs, label='$p(\\alpha)$')
 
-def P_normal_1(p):
-    P = 0
-    for i in range(0, a_1.size, 1):
-        if (p_a_1[i+1] > p):
-            i_1 = i+1
-            a_1_1 = np.interp(p, p_a_1[i:i+2:+1], a_1[i:i+2:+1])
-            break
-    for i in range(a_1.size-1, -1, -1):
-        if (p_a_1[i-1] > p):
-            i_2 = i-1
-            a_1_2 = np.interp(p, p_a_1[i:i-2:-1], a_1[i:i-2:-1])
-            break
-    P += ((a_1[i_1+1:i_2+1]-a_1[i_1:i_2])
-         *(p_a_1[i_1:i_2]+p_a_1[i_1+1:i_2+1])).sum()/2
-    P += (a_1[i_1]-a_1_1)*(p+p_a_1[i_1])/2
-    P += (a_1_2-a_1[i_2])*(p+p_a_1[i_2])/2
-    return P, a_1_1, a_1_2
+f = h5py.File('GW170817_GWTC-1.hdf5', 'r')
+dset = f['IMRPhenomPv2NRT_lowSpin_posterior']
+d_l = dset['luminosity_distance_Mpc']
+size = d_l.size**2
+sample = (np.random.choice(d_l, size=size)
+         /np.random.normal(loc=42.9, scale=3.2, size=size))**2
+hist, bin_edges = np.histogram(sample, bins='auto', density=True)
+print(bin_edges.size-1)
+hist, bin_edges = np.histogram(sample, bins=int(bin_edges.size/8), density=True)
+g, p_g = (bin_edges[:-1]+bin_edges[+1:])/2, hist
 
-def P_normal_2(p):
-    P = 0
-    for i in range(0, a_2.size, 1):
-        if (p_a_2[i+1] > p):
-            i_1 = i+1
-            a_2_1 = np.interp(p, p_a_2[i:i+2:+1], a_2[i:i+2:+1])
-            break
-    for i in range(a_2.size-1, -1, -1):
-        if (p_a_2[i-1] > p):
-            i_2 = i-1
-            a_2_2 = np.interp(p, p_a_2[i:i-2:-1], a_2[i:i-2:-1])
-            break
-    P += ((a_2[i_1+1:i_2+1]-a_2[i_1:i_2])
-         *(p_a_2[i_1:i_2]+p_a_2[i_1+1:i_2+1])).sum()/2
-    P += (a_2[i_1]-a_2_1)*(p+p_a_2[i_1])/2
-    P += (a_2_2-a_2[i_2])*(p+p_a_2[i_2])/2
-    return P, a_2_1, a_2_2
+from scipy.optimize import brentq
+def dpap(a, e, p):
+    g_ = (1+a)/(1+a*e)
+    p_g_ = np.interp(g_, g, p_g, left=0, right=0)
+    return p_g_*((1-e)/(1+a*e)**2)-p
 
-g = (np.random.choice(d_l, size=size)
-    /np.random.normal(loc=42.9, scale=3.2, size=size))**2
-ls = np.linspace(0, 3, 101)
+ls = np.sort(np.hstack((np.linspace(0, 3, 101), np.linspace(0.9, 1.0, 101))))
 as_1_1, as_1_2, as_2_1, as_2_2 = [], [], [], []
 for i in range(ls.size):
     print(i)
     l = ls[i]
     if (l == 0):
-        e = np.array(0)
+        e = np.float64(0)
     else:
-        e = np.array((1+1/l)*np.exp(-1/l))
-    a = -((1-g)/(1-g*e))
-    a = a[np.where(abs(a) < 20)]
-    rate = a.size/size
-    hist, bin_edges = np.histogram(a, bins='auto', density=True)
-    a, p_a = (bin_edges[:-1]+bin_edges[+1:])/2, hist
-    where_1, where_2 = np.where(a < -1/e), np.where(a > -1/e)
-    a_1, a_2 = a[where_1], a[where_2]
-    p_a_1, p_a_2 = p_a[where_1], p_a[where_2]
-    if (a_1.size == 0):
-        P, p, n = 0, p_a_2.max(), 0
+        e = np.float64((1+1/l)*np.exp(-1/l))
+    a = -((1-sample)/(1-sample*e))
+    a = a[(abs(a) < 10)]
+    a_ = np.linspace(-10, +10, 10001)
+    p_a_ = dpap(a_, e, 0)
+    if (l < 0.8):
+        a_max_2 = a_[(p_a_ == p_a_.max())][0]
+        P, p, n = 1, 0, 0
         while (abs(P-0.95) >= 0.0001):
             n += 1
             if (P < 0.95):
-                p -= p_a_2.max()/2**n
+                p -= p_a_.max()/2**n
             if (P > 0.95):
-                p += p_a_2.max()/2**n
-            P, a_2_1, a_2_2 = P_normal_2(p)
-            P *= rate
-            if (n == 50):
-                print(i, '!')
-                break
+                p += p_a_.max()/2**n
+            a_2_1 = brentq(dpap, np.max((-10, -1/e)), a_max_2, args=(e, p))
+            a_2_2 = brentq(dpap, a_max_2, +10, args=(e, p))
+            P = np.sum(np.all(((a > a_2_1), (a < a_2_2)), axis=0))/size
+            if (n == 50): print('!'); break
         as_2_1.append(a_2_1), as_2_2.append(a_2_2)
         i_ = i
     else:
-        P, a_2_1, a_2_2 = P_normal_2(p_a_1.max())
-        P *= rate
+        p_a_1_max = p_a_[(a_ < -1/e)].max()
+        a_max_2 = a_[(p_a_ == p_a_.max())][0]
+        a_2_1 = brentq(dpap, -1/e, a_max_2, args=(e, p_a_1_max))
+        a_2_2 = brentq(dpap, a_max_2, +10, args=(e, p_a_1_max))
+        P = np.sum(np.all(((a > a_2_1), (a < a_2_2)), axis=0))/size
         if (P > 0.95):
-            P, p, n = 0, p_a_2.max(), 0
+            P, p, n = 1, 0, 0
             while (abs(P-0.95) >= 0.0001):
                 n += 1
                 if (P < 0.95):
-                    p -= p_a_2.max()/2**n
+                    p -= p_a_.max()/2**n
                 if (P > 0.95):
-                    p += p_a_2.max()/2**n
-                P, a_2_1, a_2_2 = P_normal_2(p)
-                P *= rate
-                if (n == 50):
-                    print(i, '!')
-                    break
+                    p += p_a_.max()/2**n
+                a_2_1 = brentq(dpap, -1/e, a_max_2, args=(e, p))
+                a_2_2 = brentq(dpap, a_max_2, +10, args=(e, p))
+                P = np.sum(np.all(((a > a_2_1), (a < a_2_2)), axis=0))/size
+                if (n == 50): print('!'); break
             as_2_1.append(a_2_1), as_2_2.append(a_2_2)
             i_ = i
         else:
-            P, p, n = 0, p_a_1.max(), 0
+            a_max_1 = a_[(p_a_ == p_a_1_max)][0]
+            P, p, n = 1, 0, 0
             while (abs(P-0.95) >= 0.0001):
                 n += 1
                 if (P < 0.95):
-                    p -= p_a_1.max()/2**n
+                    p -= p_a_1_max/2**n
                 if (P > 0.95):
-                    p += p_a_1.max()/2**n
-                P_1, a_1_1, a_1_2 = P_normal_1(p)
-                P_2, a_2_1, a_2_2 = P_normal_2(p)
+                    p += p_a_1_max/2**n
+                a_1_1 = brentq(dpap, -10, a_max_1, args=(e, p))
+                a_1_2 = brentq(dpap, a_max_1, -1/e, args=(e, p))
+                a_2_1 = brentq(dpap, -1/e, a_max_2, args=(e, p))
+                a_2_2 = brentq(dpap, a_max_2, +10, args=(e, p))
+                P_1 = np.sum(np.all(((a > a_1_1), (a < a_1_2)), axis=0))/size
+                P_2 = np.sum(np.all(((a > a_2_1), (a < a_2_2)), axis=0))/size
                 P = P_1+P_2
-                P *= rate
-                if (n == 50):
-                    print(i, '!')
-                    break
-            print(a_1_1, a_1_2, a_2_1, a_2_2)
+                if (n == 50): print('!'); break
             as_1_1.append(a_1_1), as_1_2.append(a_1_2)
             as_2_1.append(a_2_1), as_2_2.append(a_2_2)
 
